9d5d395a54a116b89b7723b7376691ab912372f8
Doods <thibaultherviou@gmail.Com>
2018-10-05 10:34:27 +0200

    Add unittest

--- Doods.Framework.Std/NotifyPropertyChangedBase.cs
+++ Doods.Framework.Std/NotifyPropertyChangedBase.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Doods.Framework.Std
 {
+    /// <summary>
+    ///  NotifyPropertyChangedBase object with INotifyPropertyChanged implemented
+    /// </summary>
     public class NotifyPropertyChangedBase : INotifyPropertyChanged
     {
         /// <summary>
-        ///     Handler sur le changement d'une propriété
+        /// Occurs when property changed.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        ///     Méthode appelé lors du changement d'une propriété
+        /// Raises the property changed event.
         /// </summary>
-        /// <param name="propertyName"></param>
+        /// <param name="propertyName">Property name.</param>
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -22,18 +26,43 @@ namespace Doods.Framework.Std
 
 
         /// <summary>
-        ///     Affecte une valeur pour la propriété property
+        ///     Sets the property.
         /// </summary>
         /// <typeparam name="TProperty"></typeparam>
         /// <param name="currentValue"></param>
         /// <param name="newValue"></param>
         /// <param name="name"></param>
-        /// <param name="raise"></param>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
         protected bool SetProperty<TProperty>(ref TProperty currentValue, TProperty newValue,
-            [CallerMemberName] string name = "", bool raise = false)
+            [CallerMemberName] string name = "")
         {
-            if (EqualityComparer<TProperty>.Default.Equals(currentValue, newValue) && !raise) return false;
+
+            return SetProperty(ref currentValue, newValue, null, null, name);
+
+           
+        }
+
+
+        /// <summary>
+        ///     Sets the property.
+        /// </summary>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="currentValue"></param>
+        /// <param name="newValue"></param>
+        /// <param name="name"></param>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+        protected bool SetProperty<TProperty>(ref TProperty currentValue, TProperty newValue
+           , Action onChanged,
+            Func<TProperty, TProperty, bool> validateValue, [CallerMemberName] string name = "")
+        {
+
+            if (EqualityComparer<TProperty>.Default.Equals(currentValue, newValue))
+                return false;
+
+            if (validateValue != null && !validateValue(currentValue, newValue))
+                return false;
             currentValue = newValue;
+            onChanged?.Invoke();
             OnPropertyChanged(name);
             return true;
         }
